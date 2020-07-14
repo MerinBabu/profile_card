ff637e0a672b21039759db93f07c3d9b2c6c21ff
MerinBabu <merinkachira@gmail.com>
2020-07-14 15:06:10 +0530

    added image

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+            child: Column(
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('assets/merin.png'),
+                )
+              ],
+            )),
+      ),
 
     );
   }
